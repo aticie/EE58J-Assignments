eff1e8e5f584d7e4176967d27ce530235da25916
aticie <efehanatici@gmail.com>
2019-03-12 19:02:55 +0300

    Added color histogram

--- Assignment 1 - Product Image Recognition Challenge 2019/main.py
+++ Assignment 1 - Product Image Recognition Challenge 2019/main.py
@@ -1,25 +1,86 @@
 import os
+import sys
+import argparse
+
 import util
+
+cwd = os.getcwd()
 
 if __name__=="__main__":
 
     mainFolder = "C:\\EE58J - Data Mining for Visual Media\\Vispera-SKU101-2019\\SKU_Recognition_Dataset"
 
-    confectionery = os.path.join(mainFolder,"confectionery")
-    icecream = os.path.join(mainFolder,"icecream")
-    laundry = os.path.join(mainFolder,"laundry")
-    soft1 = os.path.join(mainFolder,"softdrinks-I")
-    soft2 = os.path.join(mainFolder,"softdrinks-II")
+    parser = argparse.ArgumentParser(description='Main script for Assignment 1.')
 
-    print("Resizing images...")
-    print("This usually takes 1 minute")
-    util.resizeBatch(confectionery)
-    print("20%")
-    util.resizeBatch(icecream)
-    print("40%")
-    util.resizeBatch(laundry)
-    print("60%")
-    util.resizeBatch(soft1)
-    print("80%")
-    util.resizeBatch(soft2)
-    print("Done!")
+    parser.add_argument('-sc', '--skipColor',
+                        help="Skip Color Histogram (use if you have .npy files that contain color hist)",
+                        action="store_true")
+
+    parser.add_argument('Data', help="Full path to the dataset's root folder")
+    
+    parser.add_argument('-sr','--skipResize', help="Skip Resize (use if you have resized images)",
+                        action="store_true")
+
+    parser.add_argument('-shog', '--skipHog',
+                        help="Skip HOG (Histogram of oriented gradients method)",
+                        action="store_true")
+    
+    args = parser.parse_args()
+    
+    mainFolder = args.Data
+
+    print(mainFolder)
+    
+    confectionery = os.path.join(mainFolder, "confectionery")
+    icecream = os.path.join(mainFolder, "icecream")
+    laundry = os.path.join(mainFolder, "laundry")
+    soft1 = os.path.join(mainFolder, "softdrinks-I")
+    soft2 = os.path.join(mainFolder, "softdrinks-II")
+
+    if args.skipResize:
+
+        print("Resizing images skipped! (Assuming they are already resized)")
+        
+    else:
+
+        print("Resizing images...")
+        print("This usually takes 1 minute")
+        util.resizeBatch(confectionery)
+        print("20%")
+        util.resizeBatch(icecream)
+        print("40%")
+        util.resizeBatch(laundry)
+        print("60%")
+        util.resizeBatch(soft1)
+        print("80%")
+        util.resizeBatch(soft2)
+        print("Done!")
+
+    if args.skipColor:
+        print("Color Histogram part skipped!")
+
+    else:
+        inputCorrect = False
+        while not inputCorrect:
+            ws = eval(input("Choose the window amount per image: "))
+            if not 128%ws == 0:
+                print("Sorry, 128 is not divisible by "+str(ws))
+                print("Please select an appropriate window amount")
+                continue
+            if ws <= 0:
+                print("Window amount cannot be zero or negative.")
+                continue
+            inputCorrect = True
+
+        print("Creating color histograms...")
+        print("[0%]-----------")
+        util.colorHist(confectionery, ws)
+        print("--[20%]--------")
+        util.colorHist(icecream, ws)
+        print("----[40%]------")
+        util.colorHist(laundry, ws)
+        print("------[60%]----")
+        util.colorHist(soft1, ws)
+        print("--------[80%]--")
+        util.colorHist(soft2, ws)
+        print("---------[100%]")
--- Assignment 1 - Product Image Recognition Challenge 2019/util.py
+++ Assignment 1 - Product Image Recognition Challenge 2019/util.py
@@ -1,6 +1,11 @@
 import cv2
 import os
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+cwd = os.getcwd()
+
 def resizeBatch(inPath):
     prevRoot = ""
     for root, dirs, files in os.walk(inPath, topdown=False):
@@ -12,7 +17,7 @@ def resizeBatch(inPath):
             if not name.endswith(".jpg"):
                 print(name+" - Doesn't end with .jpg")
                 continue
-            #print(root)
+            
             filePath = os.path.join(root,name)
             # Read image in RGB
             im = cv2.imread(filePath,1)
@@ -20,21 +25,63 @@ def resizeBatch(inPath):
             if im.shape == (128,128,3):
                 #print(name+" is already resized!")
                 continue
+            '''
+            ----DEBUG----
             # Resize image to 128x128
             # print("Resizing: "+name)
             # print("Dimensions: "+im.shape)
+            ----DEBUG----
+            '''
             newimg = cv2.resize(im,(128,128))
             # Overwrite resized image
             cv2.imwrite(filePath,newimg)
 
-'''
-#def colorHist(image, windowNr):
-    if not 128%windowNr == 0:
-        print("Sorry, 128 is not divisible by "+windowNr")
-        print("Please select an appropriate Window amount")
-        return 0
+
+def colorHist(path, windowNr):
+    prevRoot = ""
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            if name == ".DS_Store":
+                continue
+            if not root == prevRoot:
+                prevRoot = root
+                print("We are in: "+ os.path.basename(os.path.normpath(root)))
+                
+            filePath = os.path.join(root,name)
+            im = cv2.imread(filePath,1)
+            HSV_im = cv2.cvtColor(im,cv2.COLOR_BGR2HSV)
+            
+
+            stride = int(128/windowNr)
+            for i in range(windowNr):
+                for j in range(windowNr):
+                    window = im[i*stride:(i+1)*stride,j*stride:(j+1)*stride]
+                    '''
+                    ----Debug----
+                    print(window[:,:,0].shape)
+                    cv2.imshow("Window",window)
+                    cv2.waitKey(0)
+                    ----Debug----
+                    '''
+                    hHist = np.histogram(window[:,:,0])
+                    sHist = np.histogram(window[:,:,1])
+                    vHist = np.histogram(window[:,:,2])
+                    hsvHist = np.concatenate((hHist,sHist,vHist))
+                    print(hHist)
+                    plt.hist(window[:,:,0].flatten(), bins='auto')
+                    plt.title("Hue Histogram")
+                    plt.show()
+                    cv2.imshow("Window",window)
+                    cv2.waitKey(0)
+                    saveName = name.replace(".jpg","_"+str(windowNr)+"_"+str(i)+str(j)+".npy")
+                    np.save(os.path.join(root,saveName),hsvHist)
+                    
+
+            
+    print(os.path.basename(path))
+    return 0
     
-'''      
+      
 if __name__=="__main__":
     #inPath="C:\Users\Administrator\Downloads\Vispera-SKU101-2019\SKU_Recognition_Dataset\confectionery"
     resizeBatch(inPath)
