192090680a9e4c770363933ca7801eee3cc5924c
aticie <efehanatici@gmail.com>
2019-03-13 15:25:51 +0300

    some gui updates

--- Assignment 1 - Product Image Recognition Challenge 2019/main.py
+++ Assignment 1 - Product Image Recognition Challenge 2019/main.py
@@ -4,11 +4,14 @@ import argparse
 
 import util
 
+from tkinter import *
+from tkinter import filedialog
+
+
 cwd = os.getcwd()
 
-if __name__=="__main__":
-
-    mainFolder = "C:\\EE58J - Data Mining for Visual Media\\Vispera-SKU101-2019\\SKU_Recognition_Dataset"
+def doEverything(data,sr,sc,shog,ws):
+    '''mainFolder = "C:\\EE58J - Data Mining for Visual Media\\Vispera-SKU101-2019\\SKU_Recognition_Dataset"
 
     parser = argparse.ArgumentParser(description='Main script for Assignment 1.')
 
@@ -27,7 +30,9 @@ if __name__=="__main__":
     
     args = parser.parse_args()
     
-    mainFolder = args.Data
+    mainFolder = args.Data'''
+
+    mainFolder = data
 
     print(mainFolder)
     
@@ -84,3 +89,65 @@ if __name__=="__main__":
         print("--------[80%]--")
         util.colorHist(soft2, ws)
         print("---------[100%]")
+
+    if args.skipHog:
+        print("HOG Histogram part skipped!")
+        
+    else:
+        inputCorrect = False
+        while not inputCorrect:
+            ws = eval(input("Choose the window amount per image: "))
+            if not 128%ws == 0:
+                print("Sorry, 128 is not divisible by "+str(ws))
+                print("Please select an appropriate window amount")
+                continue
+            if ws <= 0:
+                print("Window amount cannot be zero or negative.")
+                continue
+            inputCorrect = True
+        util.HOGHist(confectionery,ws)
+
+def show_entry_fields():
+   print("Path: %s\n" % e1.get())
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    print(filename)
+
+def var_states():
+   print("Skip Resize: %d,\nSkip Color Hist.: %d\nSkip HOG: %d\n" % (var1.get(), var2.get(),var3.get()))
+    
+if __name__=="__main__":
+
+    master = Tk()
+
+    folder_path = StringVar()
+    lbl1 = Label(master,textvariable=folder_path)
+    lbl1.grid(row=0, column=1)
+    button2 = Button(text="Browse", command=browse_button)
+    button2.grid(row=0, column=3)
+
+    Label(master, text="Dataset root path").grid(row=0)
+
+    T = Text(master,height=1,width=50)
+    T.grid(row=1)
+    T.insert(END, cwd)
+
+    var1 = IntVar()
+    Checkbutton(master, text="Skip Resize", variable=var1).grid(row=2, sticky=W)
+    var2 = IntVar()
+    Checkbutton(master, text="Skip Color Hist.", variable=var2).grid(row=3, sticky=W)
+    var3 = IntVar()
+    Checkbutton(master, text="Skip HOG", variable=var3).grid(row=4, sticky=W)
+    Button(master, text='Quit', command=master.quit).grid(row=5,column=0, sticky=W, pady=4)
+    Button(master, text='Show', command=var_states).grid(row=5, column=0, sticky=W, padx=45, pady=4)
+
+    mainloop()
+
+
+    
+    
--- Assignment 1 - Product Image Recognition Challenge 2019/util.py
+++ Assignment 1 - Product Image Recognition Challenge 2019/util.py
@@ -57,31 +57,54 @@ def colorHist(path, windowNr):
                 for j in range(windowNr):
                     window = im[i*stride:(i+1)*stride,j*stride:(j+1)*stride]
                     '''
-                    ----Debug----
+                    ----DEBUG----
                     print(window[:,:,0].shape)
                     cv2.imshow("Window",window)
                     cv2.waitKey(0)
-                    ----Debug----
+                    ----DEBUG----
                     '''
                     hHist = np.histogram(window[:,:,0])
                     sHist = np.histogram(window[:,:,1])
                     vHist = np.histogram(window[:,:,2])
-                    hsvHist = np.concatenate((hHist,sHist,vHist))
-                    print(hHist)
-                    plt.hist(window[:,:,0].flatten(), bins='auto')
+                    hNorm = np.divide(hHist[0],np.sum(hHist[0]))
+                    sNorm = np.divide(sHist[0],np.sum(sHist[0]))
+                    vNorm = np.divide(vHist[0],np.sum(vHist[0]))
+                    hsvHist = np.concatenate((hNorm,sNorm,vNorm))
+                    '''
+                    ----DEBUG----
+                    print(hNorm)
+                    plt.hist(window[:,:,0].flatten(), bins=10)
                     plt.title("Hue Histogram")
                     plt.show()
                     cv2.imshow("Window",window)
                     cv2.waitKey(0)
+                    ----DEBUG----
+                    '''
                     saveName = name.replace(".jpg","_"+str(windowNr)+"_"+str(i)+str(j)+".npy")
                     np.save(os.path.join(root,saveName),hsvHist)
                     
+    return 0
+
+def HOGHist(path, windowNr):
+    prevRoot = ""
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            if name == ".DS_Store":
+                continue
+            if not root == prevRoot:
+                prevRoot = root
+                print("We are in: "+ os.path.basename(os.path.normpath(root)))
+
+            filePath = os.path.join(root,name)    
+            im = cv2.imread(filePath,0)
+            cv2.imshow("Grayscale",im)
+            cv2.waitKey(0)
 
             
-    print(os.path.basename(path))
+
     return 0
-    
-      
+
+            
 if __name__=="__main__":
     #inPath="C:\Users\Administrator\Downloads\Vispera-SKU101-2019\SKU_Recognition_Dataset\confectionery"
     resizeBatch(inPath)
